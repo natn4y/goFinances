fb891fac54b6a7e9d7e2e59b6e211c3e7605f6ae
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-04 07:47:03 -0300

    🔧 Definido o style global do App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+
+import theme from "./src/global/styles/theme";
+
+import { ThemeProvider } from "styled-components";
+import { Dashboard } from "./src/components/Dashboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <Text>Hello</Text>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
