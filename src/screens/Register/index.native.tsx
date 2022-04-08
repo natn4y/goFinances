578af5154fd59c4d7e24d7885e3c54af653da73e
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-08 08:08:57 -0300

    screen(Register): Header

---
+++ src/screens/Register/index.native.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  Container,
+  Header,
+  Title,
+} from './style';
+
+export function Register() {
+  return (
+    <Container>
+      <Header>
+        <Title>
+          Cadastro
+        </Title>
+      </Header>
+    </Container>
+  );
+}
