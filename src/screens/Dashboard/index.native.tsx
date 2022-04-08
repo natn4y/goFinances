6cf6b68a817b8a4117ed1767aff438a00d6676df
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-08 00:56:00 -0300

    📂 Componente Transactions

--- src/screens/Dashboard/index.native.tsx
+++ src/screens/Dashboard/index.native.tsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { Header } from "../../components/Header";
-import { HighlightCard } from "../../components/HighlightCards/HighlightCard";
-import { HighlightCards } from "../../components/HighlightCards/HighlightCards";
+
+import { Transactions, TransactionList, Title } from "../../components/Transactions/Transactions";
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from "../../components/Transactions/TransactionCard";
 
 import { Container } from "./styles.native";
+import { HighlightCard } from "../../components/HighlightCards/HighlightCard";
+import { HighlightCards } from "../../components/HighlightCards/HighlightCards";
+export interface DataListProps extends TransactionCardProps {
+  id: string;
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: "1",
+      type: "positive",
+      title: "Desenvolvimento de sites",
+      amount: "R$ 12.000,00",
+      category: { label: "Vendas", icon: "dollar-sign" },
+      date: "13/04/2020",
+    },
+    {
+      id: "2",
+      type: "negative",
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: { label: "Alimentação", icon: "coffee" },
+      date: "10/04/2020",
+    },
+    {
+      id: "3",
+      type: "negative",
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200",
+      category: { label: "Casa", icon: "shopping-bag" },
+      date: "10/04/2020",
+    },
+  ];
+
   return (
     <Container>
       <Header />
@@ -29,6 +65,14 @@ export function Dashboard() {
           lastTransaction="01 à 16 de abril"
         />
       </HighlightCards>
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   );
 }
