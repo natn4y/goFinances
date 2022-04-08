74c82f8b8214ab16d08ef12b1a05500b0ae94ef9
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-08 00:52:03 -0300

    📂 Componente HighlightCard

---
+++ src/components/HighlightCards/HighlightCard/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+// prettier-ignore
+import {
+  Container,
+  Header,
+  Title,
+  Icon,
+  Footer,
+  Amount,
+  LastTransaction
+} from "./styles";
+
+import TextTicker from "react-native-text-ticker";
+
+interface Props {
+  type: "up" | "down" | "total";
+  title: string;
+  amount: string;
+  lastTransaction: string;
+}
+
+const icon = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+  total: "dollar-sign",
+};
+
+export function HighlightCard({ type, title, amount, lastTransaction }: Props) {
+  return (
+    <Container type={type}>
+      <Header>
+        <Title type={type}>{title}</Title>
+        <Icon name={icon[type]} type={type}></Icon>
+      </Header>
+
+      <Footer>
+        <TextTicker duration={5000} loop bounce repeatSpacer={50} marqueeDelay={1000}>
+          <Amount type={type}>{amount}</Amount>
+        </TextTicker>
+        <LastTransaction type={type}>{lastTransaction}</LastTransaction>
+      </Footer>
+    </Container>
+  );
+}
--- src/screens/Dashboard/index.native.tsx
+++ src/screens/Dashboard/index.native.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import { Header } from "../../components/Header";
+import { HighlightCard } from "../../components/HighlightCards/HighlightCard";
+import { HighlightCards } from "../../components/HighlightCards/HighlightCards";
+
 import { Container } from "./styles.native";
 
 export function Dashboard() {
   return (
     <Container>
       <Header />
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighlightCards>
     </Container>
   );
 }
