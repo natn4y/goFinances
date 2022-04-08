fe6491861929dce2d0ef4a232e4efb3726232661
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-08 08:27:31 -0300

    📂 Componente Form & Input

--- src/screens/Register/index.native.tsx
+++ src/screens/Register/index.native.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
+import { Input } from "../../components/Form/Input";
+// prettier-ignore
 import {
   Container,
   Header,
   Title,
+  Form,
 } from './style';
 
 export function Register() {
   return (
     <Container>
       <Header>
-        <Title>
-          Cadastro
-        </Title>
+        <Title>Cadastro</Title>
       </Header>
+      <Form>
+        <Input placeholder="Nome" />
+        <Input placeholder="Preço" />
+      </Form>
     </Container>
   );
 }
