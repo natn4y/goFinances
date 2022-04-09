d97428f55854fc0ba3baa4a077eb8d1cc399a3f7
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-09 08:08:42 -0300

    📂 Componente TransactionsTypes

---
+++ src/components/Form/TransactionTypes/TransactionTypeButton/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { TouchableOpacityProps } from "react-native";
+// prettier-ignore
+import {
+  Container,
+  Icon,
+  Title,
+} from "./styles";
+
+const icons = {
+  up: "arrow-up-circle",
+  down: "arrow-down-circle",
+};
+
+interface Props extends TouchableOpacityProps {
+  type: "up" | "down";
+  title: string;
+  isActive: boolean;
+}
+
+export function TransactionTypeButton({ title, type, isActive, ...rest }: Props) {
+  return (
+    <Container {...rest} isActive={isActive} type={type}>
+      <Icon name={icons[type]} type={type} />
+      <Title isActive={isActive} type={type}>
+        {title}
+      </Title>
+    </Container>
+  );
+}
---
+++ src/components/Form/TransactionTypes/TransactionTypes.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const TransactionsTypes = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 8px;
+  margin-bottom: 16px;
+`;
