7d79415c47bca7a1e549dcaeeeb1838379cd4b75
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-09 08:08:42 -0300

    📂 Componente TransactionsTypes

---
+++ src/components/Form/TransactionTypes/TransactionTypeButton/styles.ts
@@ -0,0 +1,74 @@
+import styled, { css } from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { TouchableOpacity } from "react-native";
+
+interface IconProps {
+  type: "up" | "down";
+}
+
+interface ContainerProps {
+  isActive: boolean;
+  type: "up" | "down";
+}
+
+export const Container = styled(TouchableOpacity)<ContainerProps>`
+  width: 48%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-width: ${({ isActive, type }) => (isActive ? 0 : 1.5)}px;
+  border-style: solid;
+  border-color: ${({ theme }) => theme.colors.text_gray};
+  border-radius: 5px;
+  padding: 16px;
+  margin: 0 7px;
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "down" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.attention.light};
+    `};
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "up" &&
+    css`
+      background-color: ${({ theme }) => theme.colors.success.light};
+    `};
+`;
+
+export const Icon = styled(Feather)<IconProps>`
+  font-size: ${RFValue(24)}px;
+  margin-right: 12px;
+  color: ${({ theme, type }) =>
+    type === "up" ? theme.colors.success.main : theme.colors.attention.main};
+`;
+
+export const Title = styled.Text<ContainerProps>`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "up" &&
+    css`
+      color: ${({ theme }) => theme.colors.text_dark};
+    `};
+  ${({ isActive, type }) =>
+    isActive &&
+    type === "down" &&
+    css`
+      color: ${({ theme }) => theme.colors.text_dark};
+    `};
+  ${({ isActive, type }) =>
+    !isActive &&
+    type === "up" &&
+    css`
+      color: ${({ theme }) => theme.colors.text_gray};
+    `};
+  ${({ isActive, type }) =>
+    !isActive &&
+    type === "down" &&
+    css`
+      color: ${({ theme }) => theme.colors.text_gray};
+    `};
+`;
--- src/screens/Register/index.native.tsx
+++ src/screens/Register/index.native.tsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "../../components/Form/Button/Index";
 
 import { Input } from "../../components/Form/Input";
+import { TransactionsTypes } from "../../components/Form/TransactionTypes/TransactionTypes";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypes/TransactionTypeButton";
+import { CategorySelect } from "../../components/Form/TransactionTypes/CategorySelect";
 // prettier-ignore
+
 import {
   Container,
   Header,
   Title,
   Form,
+  Fields,
 } from './style';
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("");
+
+  function handleTransactionTypeButtonSelect(type: "up" | "down") {
+    setTransactionType(type);
+  }
+
   return (
     <Container>
       <Header>
         <Title>Cadastro</Title>
       </Header>
       <Form>
-        <Input placeholder="Nome" />
-        <Input placeholder="Preço" />
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+          <TransactionsTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionTypeButtonSelect("up")}
+              isActive={transactionType === "up"}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionTypeButtonSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionsTypes>
+          <CategorySelect title="Categoria" />
+        </Fields>
+        <Button title="Enviar" />
       </Form>
     </Container>
   );
