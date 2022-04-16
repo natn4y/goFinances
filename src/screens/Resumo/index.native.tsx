c2e9f2b01af653da434b3dd847d32682af5ec1cf
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-16 19:43:38 -0300

    📂 Componente HistoryCard

--- src/screens/Resumo/index.native.tsx
+++ src/screens/Resumo/index.native.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { HeaderSection } from "../../components/HeaderSection";
+import { HistoryCard } from "../../components/HistoryCard";
 import { Container } from "./styles.native";
 
 export function Resumo() {
   return (
     <Container>
       <HeaderSection title="Resumo por categoria" />
+      <HistoryCard
+        title="Compras"
+        amount="R$ 150,50"
+        color="red"
+      />
     </Container>
   );
 }
