1766dd872cb693efde2900c8024d15d4e8284307
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-17 13:43:48 -0300

    🐛 Fix: screen(Resume): Correção da ScrollView Resume >> Content

--- src/screens/Resumo/styles.native.ts
+++ src/screens/Resumo/styles.native.ts
@@ -3,9 +3,11 @@ import styled from "styled-components/native";
 export const Container = styled.View``;
 
 export const Content = styled.ScrollView.attrs({
-  contentContainerStyle: { height: "100%", padding: 24 },
+  contentContainerStyle: { padding: 24 },
   showsVerticalScrollIndicator: false,
-})``;
+})`
+  margin-bottom: auto;
+`;
 
 export const ChartContainer = styled.View`
   width: 100%;
