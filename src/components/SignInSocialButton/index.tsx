565c892ead8f49f94ba894575afaa7b54fba2c23
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-18 20:14:22 -0300

    📂 Componente SignInSocialButton

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+import { SvgProps } from "react-native-svg";
+
+import {
+  Container,
+  ImageContainer,
+  Text
+} from "./styles";
+
+interface SignInSocialButtonProps extends RectButtonProps {
+  svg: React.FC<SvgProps>;
+  title: string;
+}
+
+export function SignInSocialButton({
+  svg: Svg,
+  title,
+  ...rest
+}: SignInSocialButtonProps) {
+  return (
+    <Container>
+      <ImageContainer>
+        <Svg />
+      </ImageContainer>
+      <Text>{title}</Text>
+    </Container>
+  );
+}
