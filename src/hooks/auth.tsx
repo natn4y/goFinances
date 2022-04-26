ce505ffa089145af03b46f56ebc98188a3de142d
Natanael Araújo <natanael@hotmail.co.jp>
2022-04-26 11:47:32 -0300

    ❓⭐ Implementado Autenticação com Apple

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,6 +9,8 @@ const { CLIENT_ID } = process.env;
 const { REDIRECT_URI } = process.env;
 
 import * as AuthSession from "expo-auth-session";
+import * as AppleAuthentication from "expo-apple-authentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -26,6 +28,7 @@ interface User {
 interface IAuthContextData {
   user: User;
   signInWithGoogle: () => Promise<void>;
+  singInWithApple: () => Promise<void>;
 }
 
 interface AuthorizationResponse {
@@ -37,6 +40,7 @@ interface AuthorizationResponse {
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>({} as User);
+  const dataKey = "@gofinances:transactions";
 
   async function signInWithGoogle() {
     try {
@@ -59,16 +63,50 @@ function AuthProvider({ children }: AuthProviderProps) {
           name: userInfo.given_name,
           photo: userInfo.picture,
         });
+        await AsyncStorage.setItem(dataKey, JSON.stringify(user));
       }
     } catch (error) {
       throw new Error(error);
     }
   }
+
+  async function singInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+
+      if (credential) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+          photo: undefined,
+        };
+
+        setUser(userLogged);
+        await AsyncStorage.setItem(dataKey, JSON.stringify(userLogged));
+
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle}}>
+    <AuthContext.Provider
+      value={{
+        user,
+        signInWithGoogle,
+        singInWithApple,
+      }}
+    >
       {children}
     </AuthContext.Provider>
-  );
+  )
 }
 
 function useAuth() {
